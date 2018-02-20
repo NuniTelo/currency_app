fbb686f939e211d827d1ce87fc618f5b26985fde
Refat A <Refat A>
2018-02-20 15:50:59 +0100

    adding

--- src/main/java/currency_app/currency_app/CurrencyRepository.java
+++ src/main/java/currency_app/currency_app/CurrencyRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CurrencyRepository extends MongoRepository<currency,String>{
     public List <currency> findByType(String type);
+
 }
