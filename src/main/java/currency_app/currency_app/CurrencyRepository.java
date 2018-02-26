b535e4d1d2b5ffe88bbc82f56df683b7fb9cb51c
Refat A <Refat A>
2018-02-26 11:48:02 +0100

    sadasd

--- src/main/java/currency_app/currency_app/CurrencyRepository.java
+++ src/main/java/currency_app/currency_app/CurrencyRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface CurrencyRepository extends MongoRepository<currency,String>{
     public List <currency> findByTypeOrderByUpdateDateDesc(String type);
-    public currency findTopByOrderByUpdateDateDesc(String type);
-    public currency deleteTop1ByOrderByUpdateDateAsc();
+    public currency findTopByOrderByUpdateDateAsc(String type);
 
 }
--- src/main/java/currency_app/currency_app/helloTester.java
+++ src/main/java/currency_app/currency_app/helloTester.java
@@ -53,9 +53,9 @@ public class helloTester {
 
         DateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
         if (repository.findByTypeOrderByUpdateDateDesc(type1).size()>=50)
-            repository.deleteTop1ByOrderByUpdateDateAsc();
-        if (repository.findByTypeOrderByUpdateDateDesc(type1).size()>=50)
-            repository.deleteTop1ByOrderByUpdateDateAsc();
+            repository.delete(repository.findTopByOrderByUpdateDateAsc(type1));
+        if (repository.findByTypeOrderByUpdateDateDesc(type2).size()>=50)
+            repository.delete(repository.findTopByOrderByUpdateDateAsc(type2));
             double def1 ;double def2 ; double vl1 ; double vl2 ;
         if (type1=="USD") def1=1.1;
             else if (type1=="EUR") def1=1.3;
